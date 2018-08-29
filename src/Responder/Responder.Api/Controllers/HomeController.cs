998faca9ae113fb3f286a7320371e14082dcd434
Demetrio Alves <demetrio.alves@basecone.nl>
2018-08-29 11:28:24 +0200

    responder

---
+++ src/Responder/Responder.Api/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Responder.Service.Domain;
+
+namespace Responder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly Message _message;
+
+        public HomeController(Message message)
+        {
+            _message = message;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return _message.Content;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{message}")]
+        public void Put(string message)
+        {
+            _message.Content = message;
+        }
+    }
+}
---
+++ src/Responder/Responder.Api/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Responder.Service.Domain
+{
+    public class Message
+    {
+        public Message()
+        {
+            Content = "Hello!!";
+        }
+        public string Content { get; set; }
+    }
+}
