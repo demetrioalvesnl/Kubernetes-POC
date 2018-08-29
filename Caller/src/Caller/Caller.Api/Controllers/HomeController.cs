24b3c7cc746f3766e0081563be87f18583481071
Demetrio Alves <demetrio.alves@basecone.nl>
2018-08-29 11:16:12 +0200

    first draft

---
+++ Caller/src/Caller/Caller.Api/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Caller.Service.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caller.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly Message _message;
+
+        public HomeController(Message message)
+        {
+            _message = message;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return _message.Content;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{message}")]
+        public void Put(string message)
+        {
+            _message.Content = message;
+        }
+    }
+}
